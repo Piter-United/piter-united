4a7ebd6beabb43029fe3f2ed0e54de54802cfe9a
Mikhail Poluboyarinov <mikle.sol@gmail.com>
2018-02-25 15:17:34 +0300

    try travis build

--- src/containers/About.js
+++ src/containers/About.js
@@ -5,7 +5,8 @@ import Layout from '../components/Layout'
 const About = () => (
   <Layout>
     <div className="container pt-20">
-      <h1>This is what we're all about.</h1>
+      <h1>Piter United</h1>
+      <p>About</p>
     </div>
   </Layout>)
 
