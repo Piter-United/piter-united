642c99b1908181d9bc660a6c7a7032fd565a6c89
Mikhail Poluboyarinov <mikle.sol@gmail.com>
2018-02-25 19:27:43 +0300

    update publish

--- publish.sh
+++ publish.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
-
-git fetch
+echo "Fetch all"
+git fetch --all
+echo "Reset current branch"
 git reset --hard
+echo "Remote branch"
 git branch -r
+echo "remove tmp"
 rm -Rf .git/tmp
+echo "create tmp"
 mkdir .git/tmp
+echo "copy dist && CNAME"
 cp -r dist/ .git/tmp
 cp CNAME .git/tmp
+echo "checkout gh-pages"
 git checkout -b gh-pages
+echo "pull origin gh-pages"
 git pull origin gh-pages
+echo "copy from tmp"
 cp -r .git/tmp/ .
+echo "rm tmp"
 rm -Rf .git/tmp
+echo "add"
 git add .
+echo "commit"
 git commit -am 'build'
+echo "push"
 git push origin gh-pages
